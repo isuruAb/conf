3b45b4d7fd1e68ffc5dd588972bb8dbe8561c588
isuruAb <vpisabeywardana@gmail.com>
2023-05-06 01:26:14 +0800

    use tailwind in the app

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import TextField from '@/components/TextField'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,9 +16,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main >
-        <div className="md:container md:mx-auto">
-          <div className='md:my-auto'>Login goes here</div>
+        <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+          <div className="w-full max-w-md space-y-8">
+            <div>
+              <h2 className="text-center">Logo goes here</h2>
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+              <p className="mt-2 text-center text-sm text-gray-600">
+                Or
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+              </p>
+            </div>
+            <form className="mt-8 space-y-6" action="#" method="POST">
+              <input type="hidden" name="remember" value="true" />
+              <div>
+                <div>
+                  <TextField name="email" labelText="Email address" type="email" placeholder="Email address" required={true} />
+                </div>
+                <div className='mt-5'>
+                  <TextField name="password" labelText="Password" type="password" placeholder="Password" required={true} />
+                </div>
+
+              </div>
+
+              <div className="flex items-center justify-between">
+                <div className="flex items-center">
+                  <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600" />
+                  <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">Remember me</label>
+                </div>
+
+                <div className="text-sm">
+                  <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+                </div>
+              </div>
+
+              <div>
+                <button type="submit" className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                    <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                    </svg>
+                  </span>
+                  Sign in
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+
       </main>
     </>
   )
